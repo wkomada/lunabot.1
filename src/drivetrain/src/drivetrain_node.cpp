ac519e959f1ff084641686460525ce03b1b58637
Connor Gannaway <charlesconnorgannaway@gmail.com>
2024-09-16 18:29:52 -0400

    devcontainer setup

--- src/drivetrain/src/drivetrain_node.cpp
+++ src/drivetrain/src/drivetrain_node.cpp
@@ -1,10 +1,8 @@
 #include <memory>
-
 #include <cstdio>
 #include <iostream>
-#include <joybuttons.h>
 #include <rclcpp/rclcpp.hpp>
-#include <sensor_msgs/msg/joy.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
 #include "string"
 #include "SparkMax.hpp"
 #include "constants.h"
@@ -17,8 +15,8 @@ class Drivetrain : public rclcpp::Node
     Drivetrain()
     : Node("Drivetrain")
     {
-      subscription_ = this->create_subscription<sensor_msgs::msg::Joy>(
-      "joy", 10, std::bind(&Drivetrain::topic_callback, this, _1));
+      subscription_ = this->create_subscription<sensor_msgs::msg::JointState>(
+      "drivetrain velocity", 10, std::bind(&Drivetrain::topic_callback, this, _1));
       motors[0].SetInverted(false);
       motors[2].SetInverted(false);
     }
@@ -26,14 +24,14 @@ class Drivetrain : public rclcpp::Node
   private:
     SparkMax motors[4] = {SparkMax("can0", 1), SparkMax("can0", 2), SparkMax("can0", 3), SparkMax("can0", 4)};    
     std::string locations[4] = {"Back Left", "Back Right", "Front Left", "Front Right"};
-    void topic_callback(const sensor_msgs::msg::Joy &raw)
+    void topic_callback(const sensor_msgs::msg::JointState &drivetrain_states)
     {
-	cout << "This is working!" << endl;
-	SparkMax::Heartbeat();
-	motors[0].SetDutyCycle((raw.axes[AXIS_LEFTY])*MOTOR_MAX);
-	motors[3].SetDutyCycle((raw.axes[AXIS_LEFTY])*MOTOR_MAX);
-	motors[1].SetDutyCycle((raw.axes[AXIS_RIGHTY])*MOTOR_MAX);
-	motors[2].SetDutyCycle((raw.axes[AXIS_RIGHTY])*MOTOR_MAX);
+      cout << "This is working!" << endl;
+      SparkMax::Heartbeat();
+      motors[0].SetVelocity(drivetrain_states.velocity[0]);
+      motors[3].SetVelocity(drivetrain_states.velocity[3]);
+      motors[1].SetVelocity(drivetrain_states.velocity[1]);
+      motors[2].SetVelocity(drivetrain_states.velocity[2]);
       //for(int i = 0; i <=2; i+=2){
         //motors[i].SetDutyCycle((raw.axes[AXIS_LEFTY] + raw.axes[AXIS_RIGHTY])*MOTOR_MAX);
 	//motors[i].SetDutyCycle(0.1);
@@ -43,7 +41,7 @@ class Drivetrain : public rclcpp::Node
 	//motors[i].SetVelocity(-1);
       //}
     }
-    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
+    rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr subscription_;
 };
 
 int main(int argc, char * argv[])
---
+++ src/teleop/src/teleop_node.cpp
@@ -0,0 +1,53 @@
+#include <memory>
+
+#include <cstdio>
+#include <iostream>
+#include <joybuttons.h>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/joy.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
+#include "string"
+#include "SparkMax.hpp"
+#include "constants.h"
+using std::placeholders::_1;
+using namespace std;
+
+class Teleop : public rclcpp::Node
+{
+ public:
+    Teleop()
+    : Node("Teleop")
+    {
+      subscription_ = this->create_subscription<sensor_msgs::msg::Joy>(
+      "joy", 10, std::bind(&Teleop::topic_callback, this, _1));
+      drivetrainPub = this->create_publisher<sensor_msgs::msg::JointState>("drivetrain velocity", 10);
+
+      RCLCPP_INFO(get_logger(), "\033[1;35mMANUAL CONTROL:\033[0m \033[1;32mENABLED\033[0m");
+    }
+
+  private:
+    void topic_callback(const sensor_msgs::msg::Joy &raw)
+    {
+        sensor_msgs::msg::JointState drivetrain_states;
+        if(raw.buttons[BUTTON_LBUMPER]){
+            cout << "This is working!" << endl;
+            drivetrain_states.velocity[0] = (raw.axes[AXIS_LEFTY])*MOTOR_MAX;
+            drivetrain_states.velocity[3] = (raw.axes[AXIS_LEFTY])*MOTOR_MAX;
+            drivetrain_states.velocity[1] = (raw.axes[AXIS_RIGHTY])*MOTOR_MAX;
+            drivetrain_states.velocity[2] = (raw.axes[AXIS_RIGHTY])*MOTOR_MAX;
+        }
+        drivetrainPub->publish(drivetrain_states);
+    }
+    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
+    rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr drivetrainPub;
+
+
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<Teleop>());
+  rclcpp::shutdown();
+  return 0;
+}
